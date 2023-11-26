098f136d9887e38f4a216e9bea607da2125ef099
WingsOfCobra <anian@sollingers.de>
2023-11-26 22:43:42 +0100

    Play with Colors

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,3 @@
-// Adafruit_NeoMatrix example for single NeoPixel Shield.
-// Scrolls 'Howdy' across the matrix in a portrait (vertical) orientation.
-
 #include <Adafruit_GFX.h>
 #include <Adafruit_NeoMatrix.h>
 #include <Adafruit_NeoPixel.h>
@@ -9,12 +6,25 @@
 #define PIN 2
 #define ROWS 8
 #define COLUMNS 8
-#define PAUSE 0 // Pause in MS
+#define PAUSE 10 // DELAY IN MS
+#define CUSTOMBRIGHTNESS true
 
 long NUMPIXELS = ROWS * COLUMNS;
-long randGreen;
+
+// COLOR PALETTE
+
 long randRed;
+long randGreen;
 long randBlue;
+long BRIGHTNESS = 255;
+
+void regen_colors() {
+  BRIGHTNESS = random(0, 255);
+
+  randRed = BRIGHTNESS * random(180, 220) / 255;
+  randGreen = BRIGHTNESS * random(219, 255) / 255;
+  randBlue = BRIGHTNESS * random(244, 255) / 255;
+}
 
 Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(COLUMNS, ROWS, PIN,
   NEO_MATRIX_TOP     + NEO_MATRIX_RIGHT +
@@ -23,23 +33,20 @@ Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(COLUMNS, ROWS, PIN,
 
 void fill_pixels() {
   for (int i = 0; i < NUMPIXELS; i++) {
-    randRed = random(225);
-    randGreen = 0;
-    randBlue = 0;
+    regen_colors();
     matrix.setPixelColor(i, randRed, randGreen, randBlue);
     matrix.show();
   }
 }
 void setup() {
   matrix.begin();
+  regen_colors();
   fill_pixels();
 }
 
 
 void loop() {
-  randRed = random(225);
-  randGreen = 0;
-  randBlue = 0;
+  regen_colors();
   long randPixel = random(0, NUMPIXELS);
   matrix.setPixelColor(randPixel, randRed, randGreen, randBlue);
   matrix.show();
