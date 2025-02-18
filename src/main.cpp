3160b6d1dce5dccbee426efbb5234c1652130525
Anian Sollinger <anian@sollingers.de>
2025-02-18 17:48:53 +0100

    Main

--- src/main.cpp
+++ src/main.cpp
@@ -3,54 +3,63 @@
 #include <Adafruit_NeoPixel.h>
 
 
-#define PIN 2
+#define LED_PIN 2
 #define ROWS 8
 #define COLUMNS 8
-#define PAUSE 10 // DELAY IN MS
-#define CUSTOMBRIGHTNESS true
+#define PAUSE 1000 // DELAY IN MS
+#define BRIGHTNESS 255
+#define RANDOMBRIGHTNESS false
 
-long NUMPIXELS = ROWS * COLUMNS;
 
-// COLOR PALETTE
-
-long randRed;
-long randGreen;
-long randBlue;
-long BRIGHTNESS = 255;
-
-void regen_colors() {
-  BRIGHTNESS = random(0, 255);
-
-  randRed = BRIGHTNESS * random(180, 220) / 255;
-  randGreen = BRIGHTNESS * random(219, 255) / 255;
-  randBlue = BRIGHTNESS * random(244, 255) / 255;
-}
-
-Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(COLUMNS, ROWS, PIN,
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(COLUMNS, ROWS, LED_PIN,
   NEO_MATRIX_TOP     + NEO_MATRIX_RIGHT +
   NEO_MATRIX_COLUMNS + NEO_MATRIX_PROGRESSIVE,
   NEO_GRB            + NEO_KHZ800);
 
+
+long NUMPIXELS = ROWS * COLUMNS;
+
+long red;
+long green;
+long blue;
+
+long pixel_brightness = 191;
+
+void regenerateColors() {
+  if (RANDOMBRIGHTNESS) {
+    pixel_brightness = random(150, 255);
+  }
+  red = pixel_brightness * random(84, 100) / 255;
+  green = pixel_brightness * random(107, 140) / 255;
+  blue = pixel_brightness * random(171, 180) / 255;
+
+}
+
 void fill_pixels() {
   for (int i = 0; i < NUMPIXELS; i++) {
-    regen_colors();
-    matrix.setPixelColor(i, randRed, randGreen, randBlue);
+    regenerateColors();
+    matrix.setPixelColor(i, red, green, blue);
     matrix.show();
   }
 }
+
+void colorRandomPixel() {
+  regenerateColors();
+  long randPixel = random(0, NUMPIXELS);
+  matrix.setPixelColor(randPixel, red, green, blue);
+  matrix.show();
+  delay(PAUSE);
+}
+
 void setup() {
   matrix.begin();
-  regen_colors();
+  matrix.setBrightness(BRIGHTNESS);
+  regenerateColors();
   fill_pixels();
 }
 
-
 void loop() {
-  regen_colors();
-  long randPixel = random(0, NUMPIXELS);
-  matrix.setPixelColor(randPixel, randRed, randGreen, randBlue);
-  matrix.show();
-  delay(PAUSE);
+  colorRandomPixel();
 }
 
 
